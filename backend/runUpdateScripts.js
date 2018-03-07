19d584d2a8b9b0547700ac2139a011a329d5e209
Shane Jeffery <shane.jeffery@compli.com>
2018-03-06 20:26:51 -0800

    Finish up jobs for achievements / servers.

---
+++ backend/runUpdateScripts.js
@@ -0,0 +1,37 @@
+const CronJob = require('cron').CronJob;
+const childProcess = require('child_process');
+
+new CronJob('0 0 23 * * *', () => {
+
+        console.log('Starting CRON...');
+
+        runScript('./jobs/updateAchievements.js', (err) => {
+            if (err) throw err;
+        });
+
+        runScript('./jobs/updateRealmList.js', (err) => {
+            if (err) throw err;
+        });
+    },
+    null,
+    true,
+    'America/Los_Angeles'
+);
+
+function runScript(scriptPath, callback) {
+    let invoked = false;
+    let process = childProcess.fork(scriptPath);
+
+    process.on('error', function (err) {
+        if (invoked) return;
+        invoked = true;
+        callback(err);
+    });
+
+    process.on('exit', function (code) {
+        if (invoked) return;
+        invoked = true;
+        let err = code === 0 ? null : new Error('exit code ' + code);
+        callback(err);
+    });
+}
